622018b9a402e2ab4a7229a14af834e1bfb0f204
Bui Anh Duy <aduy8774@gmail.com>
2025-06-02 17:54:25 +0700

    before pull

--- frontend/src/Page/Home/HomePage.js
+++ frontend/src/Page/Home/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import BannerSlide from '../../components/BannerSlide/BannerSlide'
 import ComingSoon from '../../components/ComingSoonMovie/ComingSoon'
+import PlayingMovie from '../../components/PlayingMovie/PlayingMovie'
 
 function HomePage() {
   return (
     <>
       <BannerSlide></BannerSlide>
       <ComingSoon />
+      <PlayingMovie />
     </>
   )
 }
--- frontend/src/Page/OtpInput.js
+++ frontend/src/Page/OtpInput.js
@@ -46,7 +46,7 @@ function VerifyOtpForm({ registerRequest }) {
     };
 
     return (
-        <form className="text-dark p-4 rounded shadow-sm" onSubmit={ handleSubmitOtp }>
+        <form className="text-light p-4 rounded shadow-sm" onSubmit={ handleSubmitOtp }>
             <ErrorNotification>{ errorMessage }</ErrorNotification>
             <h4 className="text-center mb-3">Nhập mã OTP</h4>
             <p className="text-center text-muted">
