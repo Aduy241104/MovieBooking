e71bb87d398c59974ce39122c6ff9d188ce4a069
Bui Anh Duy <aduy8774@gmail.com>
2025-06-12 11:00:42 +0700

    thêm movie type cho dữ liệu trả về front end và chỉnh lại thuộc tính content về dạng text

--- backend/src/main/java/com/example/demo/Service/MovieScheduleService.java
+++ backend/src/main/java/com/example/demo/Service/MovieScheduleService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.DTO.response.SingleMovieDTO;
 import com.example.demo.repository.MovieRepository;
+import com.example.demo.repository.TypeRepository;
 
 @Service
 public class MovieScheduleService {
@@ -15,13 +16,27 @@ public class MovieScheduleService {
     @Autowired
     MovieRepository movieRepository;
 
+    @Autowired
+    TypeRepository typeRepository;
+
     public List<SingleMovieDTO> getNowShowingMovie(LocalDate currentDate) {
         List<SingleMovieDTO> result = movieRepository.findNowShowingMovies(currentDate);
+
+        for (SingleMovieDTO singleMovieDTO : result) {
+            List<String> types = typeRepository.findTypeNamesByMovieId(singleMovieDTO.getId());
+            singleMovieDTO.setTypes(types);
+        }
+
         return result;
     }
 
     public List<SingleMovieDTO> getCommingSoonMovie(LocalDate currentDate) {
         List<SingleMovieDTO> result = movieRepository.findUpcomingMovies(currentDate);
+
+        for (SingleMovieDTO singleMovieDTO : result) {
+            List<String> types = typeRepository.findTypeNamesByMovieId(singleMovieDTO.getId());
+            singleMovieDTO.setTypes(types);
+        }
         return result;
     }
 }
