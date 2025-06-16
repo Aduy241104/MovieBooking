9a5e306986c5ef4f3eb38bfbc71ce1e96f625b64
Bui Anh Duy <aduy8774@gmail.com>
2025-06-16 07:53:12 +0700

    ma hoa password

--- backend/src/main/java/com/example/demo/service/AuthenticationService.java
+++ backend/src/main/java/com/example/demo/service/AuthenticationService.java
@@ -6,6 +6,8 @@ import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.demo.DTO.request.AccountWithOtp;
 import com.example.demo.DTO.request.AuthenticationRequest;
@@ -37,17 +39,22 @@ import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class AuthenticationService {
+
+    @Value("${jwt.signer-key}")
+    private String SIGNER_KEY;
 
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Autowired
     RoleRepository roleRepository;
@@ -58,27 +65,24 @@ public class AuthenticationService {
     @Autowired
     OtpService otpService;
 
-    @NonFinal
-    protected static final String SIGNER_KEY = "tndQadt/oKJtCAnWMh2eq74jx2xdwHEbfKcOfeJaDDPLDayULMjWeoNGOfH9rfQA";
-
     // authentication cho table account
     public AuthRespond auth(AuthenticationRequest request) {
         Account account = accountRepository.findByEmailAndStatus(request.getUsername(), 1)
                 .orElseThrow(() -> new NotFoundException("User not found"));
 
-        boolean checkPassword = request.getPassword().equals(account.getPassword());
+        String raw = request.getPassword();
+        boolean match = passwordEncoder.matches(raw, account.getPassword());
 
-        if (!checkPassword) {
-            throw new UnauthorizedException("Invalid Password");
-        } else {
-            var token = generateToken(account);
-            AccountRespond accountRespond = accountMapper.toAccountRespond(account);
-            return AuthRespond.builder()
-                    .authenticated(true)
-                    .account(accountRespond)
-                    .token(token)
-                    .build();
+        if (!match) {
+            throw new UnauthorizedException("Invalid Password.");
         }
+        var token = generateToken(account);
+        AccountRespond accountRespond = accountMapper.toAccountRespond(account);
+        return AuthRespond.builder()
+                .authenticated(true)
+                .account(accountRespond)
+                .token(token)
+                .build();
     }
 
     // register phien ban moi nhat
@@ -103,6 +107,8 @@ public class AuthenticationService {
                 .orElseThrow(() -> new RuntimeException("Role Customer không tồn tại"));
 
         Account account = accountMapper.toAccount(registerRequest, defaultRole);
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
+
         accountRepository.save(account);
         otpService.clearOtp(accountWithOtp.getRegisterRequest().getEmail());
         return account;
@@ -135,7 +141,7 @@ public class AuthenticationService {
         JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS512);
 
         JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                .subject(account.getEmail())
+                .subject(String.valueOf(account.getAccountId()))
                 .issuer("anhduy.com")
                 .issueTime(new Date())
                 .expirationTime(new Date(Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()))
