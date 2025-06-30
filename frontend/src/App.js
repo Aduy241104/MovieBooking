001158cbe9c81f0a1a1056523302f20d7e35ee84
Bui Anh Duy <aduy8774@gmail.com>
2025-06-30 16:12:00 +0700

    fix file name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,10 +8,10 @@ import SignUpPage from './Page/AuthPage/SignUpPage';
 import { AdminLayout } from './layouts/AdminLayout/AdminLayout';
 
 
-import RoomList from './components/admin/room/RoomList';
-import CreateRoom from './components/admin/room/CreateRoom';
-import RoomDetail from './components/admin/room/RoomDetail';
-import EditRoom from './components/admin/room/EditRoom';
+import RoomList from './components/admin/Room/RoomList';
+import CreateRoom from './components/admin/Room/CreateRoom';
+import RoomDetail from './components/admin/Room/RoomDetail';
+import EditRoom from './components/admin/Room/EditRoom';
 import TypeList from './components/admin/Movie/MovieType/TypeList';
 
 
