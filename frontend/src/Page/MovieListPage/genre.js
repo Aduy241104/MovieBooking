07f2b9a244f0a6f86323d31d3bac2fb2a7254532
Huỳnh Nguyễn Phú Thành <thanhhnpce180094@fpt.edu.vn>
2025-07-21 14:16:05 +0700

    int

--- frontend/src/Page/MovieListPage/genre.js
+++ frontend/src/Page/MovieListPage/genre.js
@@ -16,11 +16,11 @@ const genreMenu = {
     items: [
         {
             key: "All",
-            label: <div onClick={ () => setGenre("All") }>Tất cả</div>,
+            label: <div onClick={ () => console.log("hello") }>Tất cả</div>,
         },
         ...GENRES.map((genre) => ({
             key: genre,
-            label: <div onClick={ () => setGenre(genre) }>{ genre }</div>,
+            label: <div onClick={ () => console.log("hello") }>{ genre }</div>,
         })),
     ],
 };
